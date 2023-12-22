4621d14d6cbf4638c745cc60efa176f363a42ec4
omarjafor <omarjaforchy@gmail.com>
2023-12-22 19:26:32 +0600

    task status drag n drop updated

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/tasks/:id', async(req, res) => {
+            const id = req.params.id;
+            const updateInfo = req.body;
+            const filter = { _id : new ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                    status : updateInfo.status
+                }
+            }
+            const result = await taskCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         app.delete('/tasks/:id', async(req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
